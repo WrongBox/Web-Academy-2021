58428feefe66c25ae8b7dab83c46abbfa82eac9b
Kevin Castillo <>
2022-05-17 16:53:00 +0000

    Initial commit

---
+++ Lab18/Lab18.js
@@ -0,0 +1,12 @@
+function hue(){
+  document.getElementById("box").style.backgroundColor="green";
+}
+function move(){
+  document.getElementById("box").style.background-color="green";
+}
+function circle(){
+  document.getElementById("box").style.background-color="green";
+}
+function size(){
+  document.getElementById("box").style.background-color="green";
+}
